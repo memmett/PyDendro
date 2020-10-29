de0410bcd4a46e7a43078cc1155d903ba40ca0f4
Matthew Emmett <memmett@gmail.com>
2020-10-28 23:12:10 -0600

    Add WIDTH column and app icon.

--- pyvroui.py
+++ pyvroui.py
@@ -12,17 +12,22 @@ import pandas as pd
 from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import QSize, Qt, QPersistentModelIndex
-from PyQt5.QtGui import QStandardItemModel
+from PyQt5.QtGui import QStandardItemModel, QIcon
+
+from pathlib import Path as path
+
+top = path(__file__).resolve().parent
 
 class HelloWindow(QMainWindow):
 
-    CORE, YEAR, MEASUREMENT = range(3)
+    CORE, YEAR, WIDTH, MEASUREMENT = range(4)
     model = None
 
     def __init__(self):
         QMainWindow.__init__(self)
 
         self.setWindowTitle("VRO")
+        self.setWindowIcon(QIcon(str(top / "leaf-fill.svg")))
         self.setCentralWidget(QWidget())
 
         self.fileName = QLabel()
@@ -74,9 +79,10 @@ class HelloWindow(QMainWindow):
 
 
     def createMeasurementsModel(self):
-        model = QStandardItemModel(0, 3, self)
+        model = QStandardItemModel(0, 4, self)
         model.setHeaderData(self.CORE, Qt.Horizontal, "Core")
         model.setHeaderData(self.YEAR, Qt.Horizontal, "Year")
+        model.setHeaderData(self.WIDTH, Qt.Horizontal, "Width")
         model.setHeaderData(self.MEASUREMENT, Qt.Horizontal, "Measurement")
         model.itemChanged.connect(self.on_item_changed)
         return model
@@ -84,9 +90,23 @@ class HelloWindow(QMainWindow):
     def add_measurement(self, value, units):
         if self.model is None:
             return
+
+        if self.model.rowCount() <= 0:
+            current = "0.0"
+        else:
+            last_core = self.model.item(0, self.CORE).text()
+            current_core = self.coreName.text()
+            if last_core != current_core:
+                current = "0.0"
+            else:
+                current = self.model.item(0, self.MEASUREMENT).text()
+        digits  = len(value.split('.')[-1])
+        fmt     = "{:.0" + str(digits) + "f}"
+        width   = fmt.format(float(value) - float(current))
         self.model.insertRow(0)
         self.model.setData(self.model.index(0, self.CORE), self.coreName.text())
         self.model.setData(self.model.index(0, self.YEAR), self.year.text())
+        self.model.setData(self.model.index(0, self.WIDTH), width)
         self.model.setData(self.model.index(0, self.MEASUREMENT), value)
         self.year.setText(str(int(self.year.text()) - 1))
         self.write()
@@ -105,6 +125,7 @@ class HelloWindow(QMainWindow):
         for r in range(self.model.rowCount()):
             data.append({ 'core': self.model.item(r, self.CORE).text(),
                           'year': self.model.item(r, self.YEAR).text(),
+                          'width': self.model.item(r, self.WIDTH).text(),
                           'measurement': self.model.item(r, self.MEASUREMENT).text() })
 
         if data:
@@ -124,6 +145,7 @@ class HelloWindow(QMainWindow):
             self.model.insertRow(r)
             self.model.setData(self.model.index(r, self.CORE), row.core)
             self.model.setData(self.model.index(r, self.YEAR), row.year)
+            self.model.setData(self.model.index(r, self.WIDTH), row.width)
             self.model.setData(self.model.index(r, self.MEASUREMENT), row.measurement)
         self.model.itemChanged.connect(self.on_item_changed)
 
@@ -170,13 +192,16 @@ def find_vro():
         except Exception as e:
             pass
     print('VRO not found!')
-    raise SystemExit
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
+    app.setApplicationName("VRO")
     mainWin = HelloWindow()
-    reader = serial.threaded.ReaderThread(serial.Serial(find_vro()), VRO)
-    reader.start()
-    reader.protocol.set_model(mainWin)
+    vro_tty = find_vro()
+    if vro_tty is not None:
+        reader = serial.threaded.ReaderThread(serial.Serial(), VRO)
+        reader.start()
+        reader.protocol.set_model(mainWin)
     mainWin.show()
     sys.exit(app.exec_())
