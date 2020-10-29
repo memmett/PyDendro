396add84b3296b8071d5e5a661fd50e846a0683d
Matthew Emmett <memmett@gmail.com>
2020-10-29 15:33:30 -0600

    Flesh out new core list.

--- pyvroui.py
+++ pyvroui.py
@@ -2,11 +2,13 @@
 # Simple GUI to record measurements from VRO
 #
 
+import collections
 import datetime
 import serial
 import serial.threaded
 import sys
 import time
+import threading
 
 import pandas as pd
 
@@ -51,6 +53,33 @@ class NewCoreDialog(QDialog):
 
         self.setLayout(vbox)
 
+Measurement = collections.namedtuple('Measurement', ['core', 'year', 'width', 'measurement'])
+
+class Measurements:
+
+    def __init__(self):
+        self.measurements = collections.defaultdict(list)
+
+    def append(self, core, year, width, value):
+        self.measurements[core].append(Measurement(core, year, width, value))
+
+    def remove_core(self, core):
+        del self.measurements[core]
+
+    def remove_last_measurement(self, core):
+        del self.measurements[core][-1]
+
+    def write(self, fname):
+        data = sum(self.measurements.values(), [])
+        if data:
+            df = pd.DataFrame([x._asdict() for x in data])
+            df.to_csv(fname, index=False)
+
+    def load(self, fname):
+        data = pd.read_csv(fname, dtype=str)
+        for row in data.itertuples():
+            self.measurements[row.core].append(Measurement(row.core, row.year, row.width, row.measurement))
+
 class VROMainWindow(QMainWindow):
 
     CORE, YEAR, WIDTH, MEASUREMENT = range(4)
@@ -63,11 +92,20 @@ class VROMainWindow(QMainWindow):
         self.setWindowIcon(QIcon(str(top / "leaf-fill.svg")))
         self.setCentralWidget(QWidget())
 
-        self.fileName = QLabel()
+        self.measurements = Measurements()
+
+        self.model = QStandardItemModel(0, 4, self)
+        self.model.setHeaderData(self.CORE, Qt.Horizontal, "Core")
+        self.model.setHeaderData(self.YEAR, Qt.Horizontal, "Year")
+        self.model.setHeaderData(self.WIDTH, Qt.Horizontal, "Width")
+        self.model.setHeaderData(self.MEASUREMENT, Qt.Horizontal, "Measurement")
+
+        self.file = QLabel()
         self.core = QLabel()
         self.year = QLineEdit()
-        self.measurements = QTableView()
-        self.measurements.setAlternatingRowColors(True)
+        self.measurements_table = QTableView()
+        self.measurements_table.setAlternatingRowColors(True)
+        self.measurements_table.setModel(self.model)
 
         self.load_button = QPushButton('Load')
         self.load_button.clicked.connect(self.load_file)
@@ -75,20 +113,24 @@ class VROMainWindow(QMainWindow):
         self.new_button = QPushButton('New')
         self.new_button.clicked.connect(self.new_file)
 
-        self.delete_button = QPushButton('Delete rows')
-        self.delete_button.clicked.connect(self.delete_rows)
+        self.delete_button = QPushButton('Delete last measurement')
+        self.delete_button.clicked.connect(self.delete_last_measurement)
 
         self.new_core_button = QPushButton('New core')
         self.new_core_button.clicked.connect(self.new_core)
 
         self.delete_core_button = QPushButton('Delete core')
+        self.delete_core_button.clicked.connect(self.delete_core)
+
+        self.core_list_widget = QListWidget()
+        self.core_list_widget.itemClicked.connect(self.core_clicked)
 
         file_group = QGroupBox("File")
         file_group_box = QHBoxLayout()
         file_group_box.addWidget(self.load_button)
         file_group_box.addWidget(self.new_button)
         file_group_box.addWidget(QLabel("File:"))
-        file_group_box.addWidget(self.fileName)
+        file_group_box.addWidget(self.file)
         file_group.setLayout(file_group_box)
 
         core_info_box = QHBoxLayout()
@@ -100,14 +142,9 @@ class VROMainWindow(QMainWindow):
         measurement_group = QGroupBox("Measurements")
         measurement_group_box = QVBoxLayout()
         measurement_group_box.addLayout(core_info_box)
-        measurement_group_box.addWidget(self.measurements)
+        measurement_group_box.addWidget(self.measurements_table)
         measurement_group_box.addWidget(self.delete_button)
         measurement_group.setLayout(measurement_group_box)
-
-        self.model = self.createMeasurementsModel()
-        self.measurements.setModel(self.model)
-
-        self.core_list_widget = QListWidget()
 
         core_group = QGroupBox("Cores")
         core_group_box = QVBoxLayout()
@@ -116,7 +153,6 @@ class VROMainWindow(QMainWindow):
         core_group_box.addWidget(self.delete_core_button)
         core_group.setLayout(core_group_box)
 
-
         vbox = QVBoxLayout()
         vbox.addWidget(file_group)
         hbox = QHBoxLayout()
@@ -124,23 +160,17 @@ class VROMainWindow(QMainWindow):
         hbox.addWidget(measurement_group)
         vbox.addLayout(hbox)
 
-        now = datetime.datetime.now()
-        self.year.setText(str(now.year))
-
         self.centralWidget().setLayout(vbox)
 
 
-    def createMeasurementsModel(self):
-        model = QStandardItemModel(0, 4, self)
-        model.setHeaderData(self.CORE, Qt.Horizontal, "Core")
-        model.setHeaderData(self.YEAR, Qt.Horizontal, "Year")
-        model.setHeaderData(self.WIDTH, Qt.Horizontal, "Width")
-        model.setHeaderData(self.MEASUREMENT, Qt.Horizontal, "Measurement")
-        model.itemChanged.connect(self.on_item_changed)
-        return model
-
     def add_measurement(self, value, units):
         if self.model is None:
+            return
+        if self.measurements is None:
+            return
+        if len(self.file.text()) == 0:
+            return
+        if len(self.core.text()) == 0:
             return
 
         if self.model.rowCount() <= 0:
@@ -155,70 +185,73 @@ class VROMainWindow(QMainWindow):
         digits  = len(value.split('.')[-1])
         fmt     = "{:.0" + str(digits) + "f}"
         width   = fmt.format(float(value) - float(current))
-        self.model.insertRow(0)
-        self.model.setData(self.model.index(0, self.CORE), self.core.text())
-        self.model.setData(self.model.index(0, self.YEAR), self.year.text())
-        self.model.setData(self.model.index(0, self.WIDTH), width)
-        self.model.setData(self.model.index(0, self.MEASUREMENT), value)
-        self.year.setText(str(int(self.year.text()) - 1))
-        self.write()
 
-    def on_item_changed(self, *args):
+        self.measurements.append(self.core.text(), self.year.text(), width, value)
         self.write()
+        self.update_measurements()
+
 
     def write(self):
-        if self.model is None:
+        if len(self.file.text()) == 0:
             return
+        self.measurements.write(self.file.text())
 
-        if len(self.fileName.text()) == 0:
-            return
 
-        data = []
-        for r in range(self.model.rowCount()):
-            data.append({ 'core': self.model.item(r, self.CORE).text(),
-                          'year': self.model.item(r, self.YEAR).text(),
-                          'width': self.model.item(r, self.WIDTH).text(),
-                          'measurement': self.model.item(r, self.MEASUREMENT).text() })
+    def update_measurements(self):
+        self.model.removeRows(0, self.model.rowCount())
 
-        if data:
-            df = pd.DataFrame(data)
-            df.to_csv(self.fileName.text(), index=False)
+        if len(self.core.text()) > 0:
+            measurements = self.measurements.measurements[self.core.text()]
+            for r, row in enumerate(measurements):
+                self.model.insertRow(0)
+                self.model.setData(self.model.index(0, self.CORE), row.core)
+                self.model.setData(self.model.index(0, self.YEAR), row.year)
+                self.model.setData(self.model.index(0, self.WIDTH), row.width)
+                self.model.setData(self.model.index(0, self.MEASUREMENT), row.measurement)
+            last = measurements[-1]
+            self.year.setText(str(int(last.year)-1))
+
+        cores = sorted(self.measurements.measurements.keys())
+        selected = self.core_list_widget.selectedItems()
+        if selected:
+            selected = self.core_list_widget.selectedItems()[0].text()
+        self.core_list_widget.clear()
+        self.core_list_widget.addItems(cores)
+        if selected:
+            try:
+                idx = cores.index(selected)
+                self.core_list_widget.setCurrentRow(idx)
+            except:
+                pass
+
 
     def load_file(self):
         fname, _ = QFileDialog.getOpenFileName(self, 'Load CSV file')
         if not fname:
             return
 
-        self.fileName.setText(fname)
-        self.model.itemChanged.disconnect()
-        self.model.removeRows(0, self.model.rowCount())
-        df = pd.read_csv(self.fileName.text(), dtype=str)
-        for r, row in enumerate(df.itertuples()):
-            self.model.insertRow(r)
-            self.model.setData(self.model.index(r, self.CORE), row.core)
-            self.model.setData(self.model.index(r, self.YEAR), row.year)
-            self.model.setData(self.model.index(r, self.WIDTH), row.width)
-            self.model.setData(self.model.index(r, self.MEASUREMENT), row.measurement)
-        self.model.itemChanged.connect(self.on_item_changed)
+        self.file.setText(fname)
+        self.core.setText('')
+        self.measurements.load(fname)
+        self.update_measurements()
+
 
     def new_file(self):
         fname, _ = QFileDialog.getSaveFileName(self, 'Create CSV file')
         if not fname:
             return
 
-        self.fileName.setText(fname)
-        self.model.itemChanged.disconnect()
-        self.model.removeRows(0, self.model.rowCount())
-        self.model.itemChanged.connect(self.on_item_changed)
+        self.file.setText(fname)
+        self.core.setText('')
+        self.measurements = Measurements()
+        self.update_measurements()
 
-    def delete_rows(self):
-        self.model.itemChanged.disconnect()
-        plist = [ QPersistentModelIndex(i) for i in
-                  self.measurements.selectionModel().selectedRows() ]
-        for i in plist:
-            self.model.removeRow(i.row())
+
+    def delete_last_measurement(self):
+        self.measurements.remove_last_measurement(self.core.text())
         self.write()
-        self.model.itemChanged.connect(self.on_item_changed)
+        self.update_measurements()
+
 
     def new_core(self):
         dlg = NewCoreDialog(self)
@@ -229,6 +262,19 @@ class VROMainWindow(QMainWindow):
             self.core_list_widget.addItem(core)
 
 
+    def delete_core(self):
+        c = self.core_list_widget.selectedItems()[0].text()
+        self.core.setText("")
+        self.measurements.remove_core(c)
+        self.write()
+        self.update_measurements()
+
+
+    def core_clicked(self):
+        c = self.core_list_widget.selectedItems()[0].text()
+        self.core.setText(c)
+        self.update_measurements()
+
 
 class VRO(serial.threaded.LineReader):
     model = None
@@ -238,6 +284,17 @@ class VRO(serial.threaded.LineReader):
         _, value, units = data.strip().split()
         if self.model is not None:
             self.model.add_measurement(value, units)
+
+
+class FakeVRO(threading.Thread):
+    model = None
+    def set_model(self, model):
+        self.model = model
+    def run(self):
+        pass
+        # while True:
+        #     self.model.add_measurement('0.0001', 'mm')
+        #     time.sleep(1)
 
 
 def find_vro():
@@ -263,5 +320,9 @@ if __name__ == "__main__":
         reader = serial.threaded.ReaderThread(serial.Serial(), VRO)
         reader.start()
         reader.protocol.set_model(main)
+    else:
+        reader = FakeVRO()
+        reader.set_model(main)
+        reader.start()
     main.show()
     sys.exit(app.exec_())
